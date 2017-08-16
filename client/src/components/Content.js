db87d61c481172cbc0dc699c6cf0e7db4a256fa8
nut077 <nuttapon.tavee@gmail.com>
2017-08-16 11:17:10 +0700

    NotFound

--- client/src/components/Content.js
+++ client/src/components/Content.js
@@ -9,10 +9,9 @@ const Content = () => (
     <div className='col-7'>
       <Switch>
         <Route exact path='/' component={Home}/> {/*exact คือการบอกว่า url ต้องตรงกันเท่านั้น*/}
-        <Route path='/categories/*' component={Categories}/>
+        <Route path='/categories' component={Categories}/>
         <Route path='/articles' component={Articles}/>
         <Route component={NotFound}/>
-        <Route/>
       </Switch>
     </div>
   </div>
