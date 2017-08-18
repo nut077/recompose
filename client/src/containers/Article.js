a72fee3dffe74bd3ec5bc8c52c6d3b6662375621
nut077 <nuttapon.tavee@gmail.com>
2017-08-18 10:11:29 +0700

    recompose

--- client/src/containers/Article.js
+++ client/src/containers/Article.js
@@ -1,12 +1,44 @@
-import React, {Component} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import {numericString} from 'airbnb-prop-types';
 import {Auth} from '../lib';
 import {Link} from 'react-router-dom';
+import {
+  compose,
+  setPropTypes,
+  withState,
+  withProps,
+  withHandlers,
+  lifecycle
+} from 'recompose'
 
-class ArticleContainer extends Component {
+const ArticleContainer = ({
+    article: {title, content},
+    id,
+    backToPreviousUrl
+  }) => (
+  <div>
+    <h2>{title}</h2>
+    <p>{content}</p>
+    <button
+      className='btn btn-primary btn-sm'
+      onClick={backToPreviousUrl}>
+      Back
+    </button>
+    {
+      Auth.getToken() && (
+        <Link
+          to={`/articles/${id}/edit`}
+          className='btn btn-sm btn-secondary'>
+          Edit
+        </Link>
+      )
+    }
+  </div>
+);
 
-  static propTypes = {
+export default compose(
+  setPropTypes({
     match: PropTypes.shape({
       params: PropTypes.shape({
         id: numericString().isRequired
@@ -15,53 +47,22 @@ class ArticleContainer extends Component {
     history: PropTypes.shape({
       goBack: PropTypes.func.isRequired
     }).isRequired
-  };
-
-  state = {
-    title: '',
-    content: ''
-  };
-
-  componentDidMount() {
-    this.loadArticle();
-  }
-
-  loadArticle() {
-    const {id} = this.props.match.params;
-
-    fetch(`/articles/${id}`)
-      .then(res => res.json())
-      .then(({article}) => this.setState({...article}))
-  }
-
-  backToPreviousUrl = () => {
-    this.props.history.goBack()
-  };
-
-  render() {
-    const {id} = this.props.match.params;
-    const {title, content} = this.state;
-    return (
-      <div>
-        <h2>{title}</h2>
-        <p>{content}</p>
-        <button
-          className='btn btn-primary btn-sm'
-          onClick={this.backToPreviousUrl}>
-          Back
-        </button>
-        {
-          Auth.getToken() && (
-            <Link
-              to={`/articles/${id}/edit`}
-              className='btn btn-sm btn-secondary'>
-              Edit
-            </Link>
-          )
-        }
-      </div>
-    )
-  }
-}
-
-export default ArticleContainer;
+  }),
+  withState('article', 'setArticle', {title: '', content: ''}),
+  withProps(props => ({id: props.match.params.id})),
+  withHandlers({
+    loadArticle: ({id, setArticle}) => _ => {
+      fetch(`/articles/${id}`)
+        .then(res => res.json())
+        .then(({article}) => setArticle(article))
+    },
+    backToPreviousUrl: ({history: {goBack}}) => _ => {
+      goBack();
+    }
+  }),
+  lifecycle({
+    componentDidMount() {
+      this.props.loadArticle();
+    }
+  })
+)(ArticleContainer);
