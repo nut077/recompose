2eb718684648875c96b427d36a5e78ca0e92f85c
nut077 <nuttapon.tavee@gmail.com>
2017-08-21 10:56:52 +0700

    complete

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {BrowserRouter as Router} from 'react-router-dom';
-import {App} from './components';
-import 'bootstrap/dist/css/bootstrap.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {BrowserRouter as Router} from 'react-router-dom'
+import {App} from './components'
+import 'bootstrap/dist/css/bootstrap.css'
 ReactDOM.render(
   <Router>
     <App/>
